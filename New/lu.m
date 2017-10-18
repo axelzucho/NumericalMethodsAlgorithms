d0a1d7a13391b1f1813333c56ddf7f00ae50e459
gadlevy <gadlevy@applint.com>
2017-10-17 19:02:17 -0500

    correccion

--- New/lu.m
+++ New/lu.m
@@ -3,11 +3,13 @@ function y=lu(matrix)
 	nColumns=size(matrix,2)
 
 	u=matrix;
-	l=eye(nRows,nColumns)
+	l=eye(nRows,nColumns);
 	i=2;
 	j=1;
+	rowToFix=0;
+	colToFix=0;
 
-	inversa=[]
+	inversa=[];
 	while i<=nRows
 
 		while j<=nColumns
@@ -16,18 +18,36 @@ function y=lu(matrix)
 				ch2=matrix(1,j);
 			end
 			
-			u(i,j)=matrix(i,j)-matrix(1,j)*(ch1/ch2);
+
+			res=matrix(i,j)-matrix(1,j)*(ch1/ch2);
+			
 			if j<=(i-1)
+				if res==0
+					u(i,j)=res;
+				else,
+					rowToFix=i;
+					colToFix=j;
+					u(i,j)=res;
+				end
+
 				l(i,j)=ch1/ch2;
+			else,
+				u(i,j)=res;
 			end
 
 			j++;
+		end
+		if(rowToFix>0)
+			[u,ch1,ch2]=fixRow(u,rowToFix,colToFix);
+			l(rowToFix,colToFix)=ch1/ch2;
+			rowToFix=0;
+			colToFix=0;
 		end
 		i++;
 		j=1;
 	end
 
-
+	
 	u
 	l
 	i=1;
@@ -40,8 +60,26 @@ function y=lu(matrix)
 		i++;
 		inversa=[inversa x];
 	end
-	inversa
 	inversa*matrix
+
+end
+
+function [matrix,ch1,ch2]=fixRow(matrix,row,column)
+	nColumns=size(matrix,2)
+
+	ch1=matrix(row,column)
+	ch2=matrix(row-1,column)
+
+	i=1;
+	while i<=nColumns
+		
+		rAct=matrix(row,i)
+		rAnt=matrix(row-1,i)
+		res=rAct-rAnt*(ch1/ch2)
+		matrix(row,i)=res;
+		i++;
+	end
+	matrix
 
 end
 
